d353a294a8d126e2fa46ca4f391dd46be306d5e5
Harshana Nishshanka <harshana@Coralielies-MacBook-Pro.local>
2022-03-03 07:42:10 +0530

    Disabled DaYZ changes

--- functions.php
+++ functions.php
@@ -41,8 +41,8 @@ add_action( 'after_setup_theme', 'tailpress_setup' );
 function tailpress_enqueue_scripts() {
 	$theme = wp_get_theme();
 
-	wp_enqueue_style( 'tailpress', get_stylesheet_directory_uri() . '/css/app.css', array(), $theme->get( 'Version' ) );
-	// wp_enqueue_script( 'tailpress', get_stylesheet_directory_uri() . '/js/app.js' , array(), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'tailpress', tailpress_asset( 'css/app.css' ), array(), $theme->get( 'Version' ) );
+	wp_enqueue_script( 'tailpress', tailpress_asset( 'js/app.js' ), array(), $theme->get( 'Version' ) );
 }
 
 add_action( 'wp_enqueue_scripts', 'tailpress_enqueue_scripts' );
@@ -261,5 +261,5 @@ function crunchify_deals_custom_post_type() {
 add_action( 'init', 'knowledge_base_custom_post_type', 0 );
 
 //DayZ Customizations 
-include get_template_directory() . '/includes/dayz-customize.php'; 
+// include get_template_directory() . '/includes/dayz-customize.php'; 
 //End of DayZ Customizations
